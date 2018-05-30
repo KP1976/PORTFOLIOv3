b9ec914d72b1aa7e9ea1ed0899b6e43738dfd8cc
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2018-05-30 20:38:29 +0200

    Poprawka boxes.js (2)
    Przy rozdzielczości większej niż 767 pikseli nie działają przyciski w sekcji "O mnie". Przy zmianie szerokości okna poprawnie zmieniają się położenia kulek w zakładkach "umiejętności" i "języki".

--- js/boxes.js
+++ js/boxes.js
@@ -3,9 +3,16 @@ const boxes = document.querySelectorAll('.box');
 const ballWidth = 16;
 
 // Funkcja przyjmuje dwa argumenty: które koło, jaki procent wiedzy
-function findBallPosition(ball, percentOfKnowledge, barWidth) {
-  let percent = (100 - percentOfKnowledge) / 100;
-  ball.style.transform = `translateX(calc(${barWidth}px - ${ballWidth / 2}px - ${barWidth * percent}px))`;
+function findBallPosition(ball, percentOfKnowledge) {
+  ball.style.left = `calc(${percentOfKnowledge}% - ${ballWidth / 2}px)`;
+}
+
+function startEvents() {
+  allBoxes.boxEdu.addEventListener('click', allBoxes.revealBoxEduWorkCourses);
+  allBoxes.boxWork.addEventListener('click', allBoxes.revealBoxEduWorkCourses);
+  allBoxes.boxCourses.addEventListener('click', allBoxes.revealBoxEduWorkCourses);
+  allBoxes.boxSkills.addEventListener('click', allBoxes.revealBoxSkills);
+  allBoxes.boxLanguages.addEventListener('click', allBoxes.revealBoxLanguages);
 }
 
 class AboutBoxes {
@@ -25,38 +32,47 @@ AboutBoxes.prototype.revealBoxEduWorkCourses = function() {
 
 AboutBoxes.prototype.revealBoxSkills = function() {
   const innerBox = this.querySelectorAll('.box-skills-languages-wrapper');
-  const barWidth = this.querySelector('.bar').offsetWidth + 20; // 20px to pading z obu stron (10 + 10)
   const balls = this.querySelectorAll('.bar-ball');
   const [ballPhotoshop, ballIndesign, ballIllustrator, ballQuarkXpress, ballHTML, ballCSS, ballJS, ballOffice] = [...balls];
 
-  findBallPosition(ballPhotoshop, 90, barWidth);
-  findBallPosition(ballIndesign, 30, barWidth);
-  findBallPosition(ballIllustrator, 40, barWidth);
-  findBallPosition(ballQuarkXpress, 90, barWidth);
-  findBallPosition(ballHTML, 60, barWidth);
-  findBallPosition(ballCSS, 70, barWidth);
-  findBallPosition(ballJS, 50, barWidth);
-  findBallPosition(ballOffice, 40, barWidth);
+  findBallPosition(ballPhotoshop, 50);
+  findBallPosition(ballIndesign, 30);
+  findBallPosition(ballIllustrator, 40);
+  findBallPosition(ballQuarkXpress, 90);
+  findBallPosition(ballHTML, 60);
+  findBallPosition(ballCSS, 70);
+  findBallPosition(ballJS, 50);
+  findBallPosition(ballOffice, 40);
 
   innerBox.forEach(box => box.classList.toggle('is-visible2'));
 };
 
 AboutBoxes.prototype.revealBoxLanguages = function() {
   const innerBox = this.querySelectorAll('.box-skills-languages-wrapper');
-  const barWidth = this.querySelector('.bar').offsetWidth + 20; // 20px to pading z obu stron (10 + 10)
   const balls = this.querySelectorAll('.bar-ball');
   const [ballPolish, ballEnglish] = [...balls];
 
-  findBallPosition(ballPolish, 95, barWidth);
-  findBallPosition(ballEnglish, 45, barWidth);
+  findBallPosition(ballPolish, 95);
+  findBallPosition(ballEnglish, 45);
 
   innerBox.forEach(box => box.classList.toggle('is-visible2'));
 };
 
 const allBoxes = new AboutBoxes(...boxes);
 
-allBoxes.boxEdu.addEventListener('click', allBoxes.revealBoxEduWorkCourses);
-allBoxes.boxWork.addEventListener('click', allBoxes.revealBoxEduWorkCourses);
-allBoxes.boxCourses.addEventListener('click', allBoxes.revealBoxEduWorkCourses);
-allBoxes.boxSkills.addEventListener('click', allBoxes.revealBoxSkills);
-allBoxes.boxLanguages.addEventListener('click', allBoxes.revealBoxLanguages);
+if (window.innerWidth < 768) {
+  startEvents();
+}
+
+window.addEventListener('resize', () => {
+  console.log(window.innerWidth);
+  if (window.innerWidth > 768) {
+    allBoxes.boxEdu.removeEventListener('click', allBoxes.revealBoxEduWorkCourses);
+    allBoxes.boxWork.removeEventListener('click', allBoxes.revealBoxEduWorkCourses);
+    allBoxes.boxCourses.removeEventListener('click', allBoxes.revealBoxEduWorkCourses);
+    allBoxes.boxSkills.removeEventListener('click', allBoxes.revealBoxSkills);
+    allBoxes.boxLanguages.removeEventListener('click', allBoxes.revealBoxLanguages);
+  } else {
+    startEvents();
+  }
+});
