68926100f194c1f7bc063c1ed0e908c0aa262b5b
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2018-10-08 15:27:17 +0200

    Add animated close button

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const menu = document.querySelector('.menu');
-const headerMyName = document.querySelector('.menu__header .header__my-name');
 const header = document.querySelector('.header');
+const headerMyName = document.querySelector('.menu__header .header__my-name');
 const menuList = document.querySelector('.menu__list');
 const menuListItem = document.querySelectorAll('.menu__list .list__item');
 
@@ -15,12 +15,16 @@ const toggleMenu = () => {
 		menuList.classList.add('show');
 		menuListItem.forEach(menuItem => menuItem.classList.add('show'));
 
+		hamburgerMenu.classList.add('close-btn');
+
 		menuIsVisible = false;
 	} else {
 		menu.classList.remove('show-menu');
 		headerMyName.classList.remove('show');
 		menuList.classList.remove('show');
 		menuListItem.forEach(menuItem => menuItem.classList.remove('show'));
+
+		hamburgerMenu.classList.remove('close-btn');
 
 		setTimeout(() => {
 			header.classList.remove('hide-header');
